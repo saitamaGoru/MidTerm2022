0c6a81a67f9a00ceb265f7b2e8a9b0dd2ce29ba5
Pritpal <pritpalsinghg679@gmail.com>
2022-10-29 18:39:39 -0400

    added online hostinf db

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1/books229"
+ // "URI": "mongodb://127.0.0.1/books229"
+ "URI": "mongodb+srv://Pritpal:A5GmbKmtfVwzAhyW@midterm2022mongodbserve.oogi7fk.mongodb.net/?retryWrites=true&w=majority"
 };
